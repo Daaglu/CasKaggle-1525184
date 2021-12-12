8ce6ac16e2b014122e3f4d3bafae6b5e5dd3c18d
Daaglu <david.aguilera.luzon@gmail.com>
2021-12-12 19:29:24 +0100

    version-1.0

---
+++ Demo/demo.py
@@ -0,0 +1,156 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import pandas as pd
+from matplotlib import pyplot as plt
+from numpy import mean
+from numpy import std
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, precision_recall_curve, \
+    average_precision_score, roc_curve, auc
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import time
+
+
+def load_dataset(path):
+    dataset = pd.read_csv(path, header=0, delimiter=',')
+    return dataset
+
+dataset_rock = load_dataset('../Data/0.csv')
+dataset_rock.columns = [
+"reading 1 sensor 1", "reading 1 sensor 2","reading 1 sensor 3","reading 1 sensor 4","reading 1 sensor 5","reading 1 sensor 6","reading 1 sensor 7", "reading 1 sensor 8",
+"reading 2 sensor 1", "reading 2 sensor 2","reading 2 sensor 3","reading 2 sensor 4","reading 2 sensor 5","reading 2 sensor 6","reading 2 sensor 7", "reading 2 sensor 8",
+"reading 3 sensor 1", "reading 3 sensor 2","reading 3 sensor 3","reading 3 sensor 4","reading 3 sensor 5","reading 3 sensor 6","reading 3 sensor 7", "reading 3 sensor 8",
+"reading 4 sensor 1", "reading 4 sensor 2","reading 4 sensor 3","reading 4 sensor 4","reading 4 sensor 5","reading 4 sensor 6","reading 4 sensor 7", "reading 4 sensor 8",
+"reading 5 sensor 1", "reading 5 sensor 2","reading 5 sensor 3","reading 5 sensor 4","reading 5 sensor 5","reading 5 sensor 6","reading 5 sensor 7", "reading 5 sensor 8",
+"reading 6 sensor 1", "reading 6 sensor 2","reading 6 sensor 3","reading 6 sensor 4","reading 6 sensor 5","reading 6 sensor 6","reading 6 sensor 7", "reading 6 sensor 8",
+"reading 7 sensor 1", "reading 7 sensor 2","reading 7 sensor 3","reading 7 sensor 4","reading 7 sensor 5","reading 7 sensor 6","reading 7 sensor 7", "reading 7 sensor 8",
+"reading 8 sensor 1", "reading 8 sensor 2","reading 8 sensor 3","reading 8 sensor 4","reading 8 sensor 5","reading 8 sensor 6","reading 8 sensor 7", "reading 8 sensor 8",
+"result"]
+dataset_scissors = load_dataset('../Data/1.csv')
+dataset_scissors.columns = [
+"reading 1 sensor 1", "reading 1 sensor 2","reading 1 sensor 3","reading 1 sensor 4","reading 1 sensor 5","reading 1 sensor 6","reading 1 sensor 7", "reading 1 sensor 8",
+"reading 2 sensor 1", "reading 2 sensor 2","reading 2 sensor 3","reading 2 sensor 4","reading 2 sensor 5","reading 2 sensor 6","reading 2 sensor 7", "reading 2 sensor 8",
+"reading 3 sensor 1", "reading 3 sensor 2","reading 3 sensor 3","reading 3 sensor 4","reading 3 sensor 5","reading 3 sensor 6","reading 3 sensor 7", "reading 3 sensor 8",
+"reading 4 sensor 1", "reading 4 sensor 2","reading 4 sensor 3","reading 4 sensor 4","reading 4 sensor 5","reading 4 sensor 6","reading 4 sensor 7", "reading 4 sensor 8",
+"reading 5 sensor 1", "reading 5 sensor 2","reading 5 sensor 3","reading 5 sensor 4","reading 5 sensor 5","reading 5 sensor 6","reading 5 sensor 7", "reading 5 sensor 8",
+"reading 6 sensor 1", "reading 6 sensor 2","reading 6 sensor 3","reading 6 sensor 4","reading 6 sensor 5","reading 6 sensor 6","reading 6 sensor 7", "reading 6 sensor 8",
+"reading 7 sensor 1", "reading 7 sensor 2","reading 7 sensor 3","reading 7 sensor 4","reading 7 sensor 5","reading 7 sensor 6","reading 7 sensor 7", "reading 7 sensor 8",
+"reading 8 sensor 1", "reading 8 sensor 2","reading 8 sensor 3","reading 8 sensor 4","reading 8 sensor 5","reading 8 sensor 6","reading 8 sensor 7", "reading 8 sensor 8",
+"result"]
+dataset_paper = load_dataset('../Data/2.csv')
+dataset_paper.columns = [
+"reading 1 sensor 1", "reading 1 sensor 2","reading 1 sensor 3","reading 1 sensor 4","reading 1 sensor 5","reading 1 sensor 6","reading 1 sensor 7", "reading 1 sensor 8",
+"reading 2 sensor 1", "reading 2 sensor 2","reading 2 sensor 3","reading 2 sensor 4","reading 2 sensor 5","reading 2 sensor 6","reading 2 sensor 7", "reading 2 sensor 8",
+"reading 3 sensor 1", "reading 3 sensor 2","reading 3 sensor 3","reading 3 sensor 4","reading 3 sensor 5","reading 3 sensor 6","reading 3 sensor 7", "reading 3 sensor 8",
+"reading 4 sensor 1", "reading 4 sensor 2","reading 4 sensor 3","reading 4 sensor 4","reading 4 sensor 5","reading 4 sensor 6","reading 4 sensor 7", "reading 4 sensor 8",
+"reading 5 sensor 1", "reading 5 sensor 2","reading 5 sensor 3","reading 5 sensor 4","reading 5 sensor 5","reading 5 sensor 6","reading 5 sensor 7", "reading 5 sensor 8",
+"reading 6 sensor 1", "reading 6 sensor 2","reading 6 sensor 3","reading 6 sensor 4","reading 6 sensor 5","reading 6 sensor 6","reading 6 sensor 7", "reading 6 sensor 8",
+"reading 7 sensor 1", "reading 7 sensor 2","reading 7 sensor 3","reading 7 sensor 4","reading 7 sensor 5","reading 7 sensor 6","reading 7 sensor 7", "reading 7 sensor 8",
+"reading 8 sensor 1", "reading 8 sensor 2","reading 8 sensor 3","reading 8 sensor 4","reading 8 sensor 5","reading 8 sensor 6","reading 8 sensor 7", "reading 8 sensor 8",
+"result"]
+dataset_ok = load_dataset('../Data/3.csv')
+dataset_ok.columns = [
+"reading 1 sensor 1", "reading 1 sensor 2","reading 1 sensor 3","reading 1 sensor 4","reading 1 sensor 5","reading 1 sensor 6","reading 1 sensor 7", "reading 1 sensor 8",
+"reading 2 sensor 1", "reading 2 sensor 2","reading 2 sensor 3","reading 2 sensor 4","reading 2 sensor 5","reading 2 sensor 6","reading 2 sensor 7", "reading 2 sensor 8",
+"reading 3 sensor 1", "reading 3 sensor 2","reading 3 sensor 3","reading 3 sensor 4","reading 3 sensor 5","reading 3 sensor 6","reading 3 sensor 7", "reading 3 sensor 8",
+"reading 4 sensor 1", "reading 4 sensor 2","reading 4 sensor 3","reading 4 sensor 4","reading 4 sensor 5","reading 4 sensor 6","reading 4 sensor 7", "reading 4 sensor 8",
+"reading 5 sensor 1", "reading 5 sensor 2","reading 5 sensor 3","reading 5 sensor 4","reading 5 sensor 5","reading 5 sensor 6","reading 5 sensor 7", "reading 5 sensor 8",
+"reading 6 sensor 1", "reading 6 sensor 2","reading 6 sensor 3","reading 6 sensor 4","reading 6 sensor 5","reading 6 sensor 6","reading 6 sensor 7", "reading 6 sensor 8",
+"reading 7 sensor 1", "reading 7 sensor 2","reading 7 sensor 3","reading 7 sensor 4","reading 7 sensor 5","reading 7 sensor 6","reading 7 sensor 7", "reading 7 sensor 8",
+"reading 8 sensor 1", "reading 8 sensor 2","reading 8 sensor 3","reading 8 sensor 4","reading 8 sensor 5","reading 8 sensor 6","reading 8 sensor 7", "reading 8 sensor 8",
+"result"]
+
+dataset_rock=dataset_rock[:500]
+dataset_scissors=dataset_scissors[:500]
+dataset_paper=dataset_scissors[:500]
+dataset_ok=dataset_ok[:500]
+
+dataset = dataset_rock.append(dataset_scissors)
+dataset = dataset.append(dataset_paper)
+dataset = dataset.append(dataset_ok)
+
+print("Dimensionalitat de la BBDD de Roca:", dataset_rock.shape)
+print("Dimensionalitat de la BBDD de Tissores:", dataset_scissors.shape)
+print("Dimensionalitat de la BBDD de Paper:", dataset_paper.shape)
+print("Dimensionalitat de la BBDD de Ok:", dataset_ok.shape)
+print("Dimensionalitat de la BBDD:", dataset.shape)
+
+print(dataset.isnull().values.any())
+print(dataset.isnull().sum().sum())
+
+data_mean, data_std = mean(dataset.values), std(dataset.values)
+print(data_mean, data_std)
+
+
+
+data = dataset.values
+
+x = data[:, :64]
+y = data[:, 64]
+
+print(x)
+print(y)
+
+particions = [0.3, 0.5, 0.7, 0.8]
+kernel_lst = ['linear', 'poly', 'rbf', 'sigmoid']
+
+for kern in kernel_lst:
+    inici = time.time()
+    print("---------- KERNEL:", kern, " ---------- ")
+    for part in particions:
+        x_t, x_v, y_t, y_v = train_test_split(x, y, train_size=part)
+
+        logireg = LogisticRegression(C=1.0, fit_intercept=True, penalty='l2', max_iter = 1000)
+        logireg.fit(x_t, y_t)
+
+        print("Correct classification Logistic ", part, "% of the data: ", logireg.score(x_v, y_v))
+        fi = time.time()
+        total = fi - inici
+        print("Ha tardat: ", total, " segons")
+plt.show()
+
+print("Decision tree")
+for part in particions:
+    inici = time.time()
+    x_t, x_v, y_t, y_v = train_test_split(x, y, train_size=part)
+    clf = DecisionTreeClassifier(random_state=0)
+    clf.fit(x_t, y_t)
+    cv = cross_val_score(clf, x_v, y_v, cv=10)
+    total = 0
+    for v in cv:
+        total += v
+    print("Mean accuracy with ", part, "% of the data: ", round(total/10, 4))
+    fi = time.time()
+    total = fi - inici
+    print("Ha tardat: ", total, " segons")
+    plot_tree(clf)
+probs = clf.predict_proba(x_v)
+
+print("KNN")
+
+x_t, x_v, y_t, y_v = train_test_split(x, y, train_size=0.7)
+
+neigh_array = range(1, 10)
+
+for n in neigh_array:
+    inici = time.time()
+    print("---------- num neighbors = ", n, " ---------- ");
+
+    model = KNeighborsClassifier(n_neighbors=n)
+
+    model.fit(x_t,y_t)
+
+    predictions = model.predict(x_t)
+
+    acc = accuracy_score(y_t, predictions)
+    prec = precision_score(y_t, predictions, average='micro')
+    rec = recall_score(y_t, predictions, average='micro')
+    conf_mat = confusion_matrix(y_t, predictions).T
+    fi = time.time()
+    total = fi - inici
+    print(f'Accuracy:{acc}')
+    print(f'Precision:{prec}')
+    print(f'Recall:{rec}')
+    print("Ha tardat: ", total, " segons")
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# CasKaggle-1525184
-https://www.kaggle.com/kyr7plus/emg-4
+# Pràctica Kaggle APC UAB 2021-22
+### Nom: David Aguilera Luzón
+### DATASET: Classify gestures by reading muscle activity.
+### URL: [kaggle] https://www.kaggle.com/kyr7plus/emg-4
+
+## Resum
+El
+dataset utilitza les dades recollides per 8 sensors col·locats al braç d'una persona en diferents posicions mentres aquesta realitzava un dels següents gestos:
+- pedra (puny tancat)
+- tissora (dit índex i mig estirats)
+- paper (tots els dits estirats)
+- ok (polze i índex tocant-se, la resta tancats)
+
+Les mesures es van prendre en 8 tandes.
+Per tant tindre 1 columna per a cada sensor durant cada una de les tandes, és a dir, tindrem 64 columnes.
+Això més una columna que indiqui el gest resultant: 0 per roca, 1 per tissora, 2 per paper i 3 per ok.
+Per el que fa a les files, es van prendre les mesures mantenint el gest 6 vegades durant 20 segons per a cada tanda, i es prenia un registre cada 40ms.
+Per tant s'han registrat 3000 mesures per a cada sensor.
+És a dir, hiha 3000 files per a cada gest.
+Per tant, el dataset serà de 65 columnes x a 12000 files.
+
+### Objectius del dataset
+Poder classificar de quin gest es tracta a partir de l'entrada dels sensors.
+
+### Experiments
+Durant aquesta pràctica he provat els següents algoritmes:
+KNN, Decission-tree i Classificació logísitca amb 4 tipus de kernel.
+
+### Preprocessat
+Com les dades venien donades en 4 arxius diferents, primer he necessitat juntar-les totes en un sol dataset, per a poder entrenar els classificadors correctament.
+Ja que si només ho feia amb un dels arxius solament podria classificar auqell gest en concret.
+Per això els he ajuntat i per a facilitar treballar amb les dades he afegit headers a les columnes.
+Després he comprovat la dimensionalitat del dataset generat per veure que és com toca.
+Hi ha menys columnes de les esperades, per això també miro el número de mesures per a cada gest i trobo que no hi ha cap que arribi a les 3000.
+Això es deu a algún error al moment de prendre les mesures, de totes maneres, com tots els gestos tenen aproximadament el mateix número de mostres, no crec que calgui afegir ni borrar dades de cap gest.
+En total tenim: 65 columnes x 11674 columnes.
+
+Després vaig mirar si teniem valors nulls per a borrar-los, però no hi havia.
+Finalment vaig comprovar la desviació estándard que és de: 17.23%
+Com és un número relativament baix indica que els valors estan agrupats, per això crec que no val la pena buscar els outliers ni borrar-los, ja que són pocs i de tota manera poden ajudar a entrenar per classificar millor.
+
+### Model
+Models:
+|Model|Hiperparametre|Mètrica|Temps segons|
+Logistic Regression Kernel = linear|train_size = [0.3,0.5,0.7,0.8], C=1.0, fit_intercept=True, penalty='l2'|score=[0.34,0.35,0.35,0.33]|temps=[0.49,1.35,3.07,5.23]
+Logistic Regression Kernel = poly|train_size = [0.3,0.5,0.7,0.8], C=1.0, fit_intercept=True, penalty='l2'|score=[0.33,0.34,0.34,0.34]|temps=[0.64,1.84,3.84,5.76]
+Logistic Regression Kernel = rbf|train_size = [0.3,0.5,0.7,0.8], C=1.0, fit_intercept=True, penalty='l2'|score=[0.34,0.35,0.35,0.33]|temps=[0.41,1.20,2.69,4.45]
+Logistic Regression Kernel = sigmoid|train_size = [0.3,0.5,0.7,0.8], C=1.0, fit_intercept=True, penalty='l2'|score=[0.34,0.33,0.36,0.33]|temps=[0.42,1.23,3.03,5.06]
+
+Decission Tree|train_size = [0.3,0.5,0.7,0.8], random_state=0|mean accuracy=0.77|temps=[4.60,3.02,2.00,1.42]
+
+KNN|train_size=0.7, numero vecinos = [1,2,3,4,5,6,7,8,9]|Accuracy = [1,0.84,0.78,0.77,0.75,0.73,0.73,0.72,0.71], Precission = [1,0.84,0.78,0.77,0.75,0.73,0.73,0.72,0.72], Recall = [1,0.84,0.78,0.77,0.75,0.73,0.73,0.72,0.71]|temps = [0.61,9.2,9.3,9.4,9.1,9.4,9.4,9.4,10.7]
+
+
+## Demo
+Per tal de fer una prova és pot fer:
+
+## Conclusions
+El millor model que he trobat és el Decission-tree amb un train-size del 30% de les dades.
+De fet quan major el train-size pitjor classifica.
+La resta de models provats classifican quasi igual de bé, exceptuant la Regressió Logística que té una performance del 30% en general.
+En el cas del KNN amb pocs veïns també dona molt bon resultat però clar, pot ser atzar degut a que casualment triï el veí correctes.
+És per això que amb KNN el resultats que mirem són els dels 6-7-8-9 veïns, i tot i ser bons el decission-tree segueix sent el millor.
+
+Del Decission-tree he mirat les corves de Precision-Recall i ROC curve. Classifica aproximadament igual totes les classes però té problemes per fer-ho amb la classe de paper.
+Crec que la classe que millor classifica és roca ja que és la que té els dits més diferents respectivament als altres gestos, ja que estan tots tancats.
+En canvi, tissora té molta semblança amb l'ok i paper, ja que té dits oberts i això crec que pot fer que tingui valors semblants i per tant costa de classificar.
+![img.png](images/img.png)
